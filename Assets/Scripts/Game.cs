6815998ac024b720867d679e26565855eee1d95a
Nujerk <junkerlucas@gmail.com>
2016-05-07 00:35:55 +0200

    Simple Game Handling script

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,17 @@
+﻿using roastedrooster.chickenrun.laws;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Game : MonoBehaviour
+    {
+        public void Start()
+        {
+
+        }
+    }
+}
