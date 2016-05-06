5a15c27d1f802ee2c85c97b01b4a4ecc89e6125c
Nujerk <junkerlucas@gmail.com>
2016-05-07 01:38:06 +0200

    Add UI behaviour

---
+++ Assets/Scripts/UI/DisplayRule.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using roastedrooster.chickenrun.laws;
+using UnityEngine.UI;
+
+namespace roastedrooster.chickenrun.ui
+{
+    public class DisplayRule : MonoBehaviour
+    {
+        public Text ruleName;
+        public Text ruleTime;
+
+        void Update()
+        {
+            var lawManager = LawManager.Instance;
+            if(lawManager.AppliedLaw != null)
+            {
+                var text = "Rule : " + lawManager.AppliedLaw.name;
+                var remainingTime = lawManager.RemainingTimeForLaw;
+                ruleName.text = text;
+                ruleTime.text = Mathf.RoundToInt(remainingTime) + "s";
+            }
+            
+        }
+    }
+}
