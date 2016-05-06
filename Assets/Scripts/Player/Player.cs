29f1cb6f09d8d4dc5ca6bc50383f2cc1ffb0b504
Nujerk <junkerlucas@gmail.com>
2016-05-07 00:36:57 +0200

    Player script

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace roastedrooster.chickenrun.player
+{
+    public class Player : MonoBehaviour
+    {
+        public string name;
+        public void Punished()
+        {
+            Debug.Log(name + "has been punished !");
+        }
+    }
+}
