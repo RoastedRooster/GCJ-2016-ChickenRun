d79089803f87e32dd2a936d592c63674299412b9
Nujerk <junkerlucas@gmail.com>
2016-05-07 00:37:31 +0200

    Law & Law manager

---
+++ Assets/Scripts/Laws/Law.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace roastedrooster.chickenrun.laws
+{
+    public enum TriggeringEventID
+    {
+        PlayerJump
+    }
+
+    public class Law : ScriptableObject
+    {
+        public string name;
+        public TriggeringEventID eventID;
+    }
+}
---
+++ Assets/Scripts/Laws/LawManager.cs
@@ -0,0 +1,29 @@
+﻿using roastedrooster.chickenrun.player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace roastedrooster.chickenrun.laws
+{
+    public class LawManager : ScriptableObject
+    {
+        public List<Law> availableLaws;
+        private List<Law> _appliedLaws;
+
+        public LawManager()
+        {
+            _appliedLaws = new List<Law>();
+        }
+
+        public void PlayerEvent(TriggeringEventID eventID, Player player)
+        {
+            if(_appliedLaws.Count(law => law.eventID == eventID) > 0)
+            {
+                player.Punished();
+            }
+        }
+
+    }
+}
