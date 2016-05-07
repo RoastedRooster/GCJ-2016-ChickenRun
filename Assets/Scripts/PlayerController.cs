e808ecd81e14edbfaa9b2427ecef64c0c5760df7
Clement-R <rondeau.clement@gmail.com>
2016-05-07 12:11:43 +0200

    Work on wall slide and wall jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,9 @@ namespace Parallax
         public int jumpVelocity = 5;
         public int maxFrameHoldingJump = 20;
 
+        bool grounded = true;
+        bool wallSliding = false;
+
         private Rigidbody2D rb2d;
         private int frameHoldingJump = 0;
 
@@ -18,7 +21,7 @@ namespace Parallax
 
         void Update()
         {
-            if((frameHoldingJump >= 0 && !Input.GetKey(KeyCode.Space)) || frameHoldingJump >= maxFrameHoldingJump)
+            if(((frameHoldingJump >= 0 && !Input.GetKey(KeyCode.Space)) || frameHoldingJump >= maxFrameHoldingJump) && grounded)
             {
                 gameObject.GetComponent<Rigidbody2D>().AddForce(frameHoldingJump * Vector2.up * jumpVelocity / maxFrameHoldingJump, ForceMode2D.Impulse);
                 frameHoldingJump = 0;
@@ -30,11 +33,66 @@ namespace Parallax
             }
 
             // Debug.Log(rb2d.velocity.y);
+
+            Debug.DrawLine(transform.position, new Vector2(0, -1), Color.red);
         }
 
         void FixedUpdate() {
             float h = Input.GetAxisRaw("Horizontal");
             rb2d.velocity = new Vector2(h *maxSpeed,rb2d.velocity.y);
+        }
+
+        void OnCollisionStay2D(Collision2D collider) {
+            checkIfGrounded();
+            checkIfSliding();
+        }
+
+        void OnCollisionExit2D(Collision2D collider) {
+            grounded = false;
+            wallSliding = false;
+        }
+
+        private void checkIfSliding() {
+            /*
+            RaycastHit2D[] hits;
+
+            Vector2 positionToCheck = transform.position;
+
+            // Check on left
+            hits = Physics2D.RaycastAll(positionToCheck, new Vector2(1, 0), 0.01f);
+            // if a collider was hit, we are grounded
+            if (hits.Length > 0) {
+                Debug.Log("Against wall");
+            }
+
+            // Check on right
+            hits = Physics2D.RaycastAll(positionToCheck, new Vector2(-1, 0), 0.01f);
+            // if a collider was hit, we are grounded
+            if (hits.Length > 0) {
+                Debug.Log("Against wall");
+            }
+            */
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector2.right, out hit)) {
+                float distanceToRightWall = hit.distance;
+                if (distanceToRightWall <= 32 + 0.01f) {
+                    Debug.Log("i'm hitting the right hand wall!");
+                    // Place logic for hitting right wall here
+                }
+            }
+        }
+
+        private void checkIfGrounded() {
+            RaycastHit2D[] hits;
+
+            // We raycast down 1 pixel from this position to check for a collider
+            Vector2 positionToCheck = transform.position;
+            hits = Physics2D.RaycastAll(positionToCheck, new Vector2(0, -1), 0.01f);
+
+            // if a collider was hit, we are grounded
+            if (hits.Length > 0) {
+                grounded = true;
+            }
         }
     }
 }
